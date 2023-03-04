062394ad1b849a2bda9c4a40ea78c70019c7d229
quantumiracle <zhding@mail.ustc.edu.cn>
2023-03-03 22:02:54 -0500

    fix chat func

--- scripts/chatgpt/query.py
+++ scripts/chatgpt/query.py
@@ -2,8 +2,8 @@ from chatgpt_wrapper import ChatGPT
 from text2audio import text_to_audio
 
 bot = ChatGPT()
-# response = bot.ask("Hello, world!")
-response = bot.ask("Imagine we are working with a household robot. The job of this robot is to make an omelette. The objects available around are: fridge, bowl, pan, oil, stove The main functions you can use are: locate_object(object_name): Returns the XYZ coordinates of an object of interest. go_to_location(object_name): Moves robot to a location specified by XYZ coordinates. Returns nothing. pick_up(object_name): Picks up the object of interest. Returns nothing. use_item(object_name): Takes the name of an object as input. Performs an appropriate action on the object at hand (pour oil, crack eggs). Returns nothing. Can you make use of these to write code to go to the kitchen and make an omelette? Try to use these low level functions to construct more high level behaviors and then solve the task using those high level primitives.")
+response = bot.ask("Hello, world!")
+# response = bot.ask("Imagine we are working with a household robot. The job of this robot is to make an omelette. The objects available around are: fridge, bowl, pan, oil, stove The main functions you can use are: locate_object(object_name): Returns the XYZ coordinates of an object of interest. go_to_location(object_name): Moves robot to a location specified by XYZ coordinates. Returns nothing. pick_up(object_name): Picks up the object of interest. Returns nothing. use_item(object_name): Takes the name of an object as input. Performs an appropriate action on the object at hand (pour oil, crack eggs). Returns nothing. Can you make use of these to write code to go to the kitchen and make an omelette? Try to use these low level functions to construct more high level behaviors and then solve the task using those high level primitives.")
 
 # Open a file for writing
 with open('test.txt', 'w') as f:
--- scripts/chatgpt/text2audio.py
+++ scripts/chatgpt/text2audio.py
@@ -3,6 +3,7 @@ import os
 from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
+from pydub import effects
 
 def text_to_audio(text):
     audio_path = "cache/test.mp3"
@@ -12,6 +13,7 @@ def text_to_audio(text):
     tts.save(audio_path)
     # load the audio file using pydub
     sound = AudioSegment.from_file(audio_path, format="mp3")
+    sound = sound.speedup(1.3, 150, 25)
     # play the audio file
     play(sound)
     os.remove(audio_path)
